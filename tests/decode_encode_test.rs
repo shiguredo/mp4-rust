be95d9f5c744c84a9c5386c7ec402f8b54e01c46
Takeru Ohta <phjgt308@gmail.com>
2024-09-20 16:21:40 +0900

    opus 用のテストデータを追加

--- tests/decode_encode_test.rs
+++ tests/decode_encode_test.rs
@@ -24,3 +24,28 @@ fn decode_encode_black_h264_video_mp4() -> Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn decode_encode_beep_opus_audio_mp4() -> Result<()> {
+    let input_bytes = include_bytes!("testdata/beep-opus-audio.mp4");
+    let file: Mp4File = Mp4File::decode(&mut &input_bytes[..])?;
+
+    // デコード時に未処理のボックスがないことを確認する。
+    assert_eq!(
+        file.iter_unknown_boxes().map(|x| x.0).collect::<Vec<_>>(),
+        Vec::new()
+    );
+
+    let mut output_bytes = Vec::new();
+    file.encode(&mut output_bytes)?;
+
+    // エンコード結果をデコードしたら同じ MP4 になっていることを確認する。
+    let encoded_file: Mp4File = Mp4File::decode(&mut &output_bytes[..])?;
+    assert_eq!(file, encoded_file);
+
+    // エンコード結果のバイト列が正しいことを確認する。
+    assert_eq!(input_bytes.len(), output_bytes.len());
+    assert_eq!(&input_bytes[..], output_bytes);
+
+    Ok(())
+}
