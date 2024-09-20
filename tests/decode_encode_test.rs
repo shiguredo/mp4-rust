fb4ad6daed7048c031de959406fd3f4fd84fe4e6
Takeru Ohta <phjgt308@gmail.com>
2024-09-20 14:54:43 +0900

    Rename test file

--- tests/decode_encode_test.rs
+++ tests/decode_encode_test.rs
@@ -10,3 +10,14 @@ fn decode_black_h264_video_mp4() -> Result<()> {
     );
     Ok(())
 }
+
+#[test]
+fn encode_black_h264_video_mp4() -> Result<()> {
+    let input_bytes = include_bytes!("testdata/black-h264-video.mp4");
+    let file = Mp4File::<RootBox>::decode(&mut &input_bytes[..])?;
+    assert_eq!(
+        file.iter_unknown_boxes().map(|x| x.0).collect::<Vec<_>>(),
+        Vec::new()
+    );
+    Ok(())
+}
