3676e33cdd1a6635bfa970ddd856bd5db7a803d5
Takeru Ohta <phjgt308@gmail.com>
2024-09-24 13:00:36 +0900

    av1 用のテストを追加

--- tests/decode_encode_test.rs
+++ tests/decode_encode_test.rs
@@ -50,6 +50,33 @@ fn decode_encode_black_vp9_video_mp4() -> Result<()> {
 }
 
 #[test]
+fn decode_encode_black_av1_video_mp4() -> Result<()> {
+    let input_bytes = include_bytes!("testdata/black-av1-video.mp4");
+    let file: Mp4File = Mp4File::decode(&mut &input_bytes[..])?;
+
+    // デコード時に未処理のボックスがないことを確認する。
+    assert_eq!(collect_unknown_box_types(&file), Vec::new());
+
+    let mut output_bytes = Vec::new();
+    file.encode(&mut output_bytes)?;
+
+    // エンコード結果をデコードしたら同じ MP4 になっていることを確認する。
+    let encoded_file: Mp4File = Mp4File::decode(&mut &output_bytes[..])?;
+    assert_eq!(file, encoded_file);
+
+    // エンコード結果のバイト列が正しいことを確認する。
+    assert_eq!(input_bytes.len(), output_bytes.len());
+
+    // // ボックスの順番は入れ替わるのでソートした結果を比較する
+    // let mut input_bytes = input_bytes.to_vec();
+    // input_bytes.sort();
+    // output_bytes.sort();
+    assert_eq!(&input_bytes[..], output_bytes);
+
+    Ok(())
+}
+
+#[test]
 fn decode_encode_beep_opus_audio_mp4() -> Result<()> {
     let input_bytes = include_bytes!("testdata/beep-opus-audio.mp4");
     let file: Mp4File = Mp4File::decode(&mut &input_bytes[..])?;
