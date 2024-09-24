1813669b02dff771eb8bb2d730d9c7679abd8af5
Takeru Ohta <phjgt308@gmail.com>
2024-09-24 14:08:39 +0900

    h265 用のテストを追加

--- tests/decode_encode_test.rs
+++ tests/decode_encode_test.rs
@@ -23,6 +23,28 @@ fn decode_encode_black_h264_video_mp4() -> Result<()> {
 }
 
 #[test]
+fn decode_encode_black_h265_video_mp4() -> Result<()> {
+    let input_bytes = include_bytes!("testdata/black-h265-video.mp4");
+    let file: Mp4File = Mp4File::decode(&mut &input_bytes[..])?;
+
+    // デコード時に未処理のボックスがないことを確認する。
+    assert_eq!(collect_unknown_box_types(&file), Vec::new());
+
+    let mut output_bytes = Vec::new();
+    file.encode(&mut output_bytes)?;
+
+    // エンコード結果をデコードしたら同じ MP4 になっていることを確認する。
+    let encoded_file: Mp4File = Mp4File::decode(&mut &output_bytes[..])?;
+    assert_eq!(file, encoded_file);
+
+    // エンコード結果のバイト列が正しいことを確認する。
+    assert_eq!(input_bytes.len(), output_bytes.len());
+    assert_eq!(&input_bytes[..], output_bytes);
+
+    Ok(())
+}
+
+#[test]
 fn decode_encode_black_vp9_video_mp4() -> Result<()> {
     let input_bytes = include_bytes!("testdata/black-vp9-video.mp4");
     let file: Mp4File = Mp4File::decode(&mut &input_bytes[..])?;
