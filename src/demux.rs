b7165611cff19d1ef9f41e86bafb1b005a4d8d5b
Takeru Ohta <phjgt308@gmail.com>
2025-10-22 14:05:18 +0900

    feat: Sample構造体とMp4FileDemuxer Iterator実装を追加

--- src/demux.rs
+++ src/demux.rs
@@ -6,6 +6,17 @@ use std::collections::VecDeque;
 #[cfg(not(feature = "std"))]
 use alloc::collections::VecDeque;
 
+use crate::{Result, boxes::SampleEntry};
+
+#[derive(Debug, Clone)]
+pub struct Sample {
+    pub track_id: u32,
+    pub sample_entry: SampleEntry,
+    pub timestamp: u64,
+    pub duration: u32,
+    pub data: Vec<u8>,
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Mp4FileDemuxerAction {
     Read { size: usize, position: Option<u64> },
@@ -25,5 +36,13 @@ impl Mp4FileDemuxer {
 
     pub fn next_action(&mut self) -> Option<Mp4FileDemuxerAction> {
         self.action_queue.pop_front()
+    }
+}
+
+impl Iterator for Mp4FileDemuxer {
+    type Item = Result<Sample>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!()
     }
 }
