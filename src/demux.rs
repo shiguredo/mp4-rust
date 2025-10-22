52c4c0c2baf849cc3873710822122b84ae0d9eae
Takeru Ohta <phjgt308@gmail.com>
2025-10-22 12:55:01 +0900

    feat: Mp4FileDemuxerに action_queue と next_action メソッドを追加

--- src/demux.rs
+++ src/demux.rs
@@ -1,9 +1,29 @@
 #![allow(missing_docs)]
 
+#[cfg(feature = "std")]
+use std::collections::VecDeque;
+
+#[cfg(not(feature = "std"))]
+use alloc::collections::VecDeque;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Mp4FileDemuxerAction {
     Read { size: usize, position: Option<u64> },
 }
 
-#[derive(Debug)]
-pub struct Mp4FileDemuxer {}
+#[derive(Debug, Default)]
+pub struct Mp4FileDemuxer {
+    action_queue: VecDeque<Mp4FileDemuxerAction>,
+}
+
+impl Mp4FileDemuxer {
+    pub fn new() -> Self {
+        Self {
+            action_queue: VecDeque::new(),
+        }
+    }
+
+    pub fn next_action(&mut self) -> Option<Mp4FileDemuxerAction> {
+        self.action_queue.pop_front()
+    }
+}
