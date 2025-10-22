1652962d40149c4c70d85f73f73bd16eab1e27a6
Takeru Ohta <phjgt308@gmail.com>
2025-10-22 12:47:38 +0900

    feat: Mp4FileDemuxerAction列挙型を追加

--- src/demux.rs
+++ src/demux.rs
@@ -1,5 +1,9 @@
-//! TODO: doc
+#![allow(missing_docs)]
 
-/// TODO: doc
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Mp4FileDemuxerAction {
+    Read { size: usize, position: Option<u64> },
+}
+
 #[derive(Debug)]
 pub struct Mp4FileDemuxer {}
