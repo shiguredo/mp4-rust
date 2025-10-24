a7bb8470cbf908cc9975f37e709a3ee82cdeb92d
Takeru Ohta <phjgt308@gmail.com>
2025-10-24 10:13:45 +0900

    feat: Mp4FileMuxer と Mp4FileMuxerOptions を実装する

--- src/mux.rs
+++ src/mux.rs
@@ -1,1 +1,29 @@
-#![expect(missing_docs)]
+#![expect(missing_docs, dead_code)]
+
+#[derive(Debug, Clone)]
+pub struct Mp4FileMuxerOptions {
+    pub reserved_moov_box_size: usize,
+}
+
+impl Default for Mp4FileMuxerOptions {
+    fn default() -> Self {
+        Self {
+            reserved_moov_box_size: 0,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Mp4FileMuxer {
+    options: Mp4FileMuxerOptions,
+}
+
+impl Mp4FileMuxer {
+    pub fn new() -> Self {
+        Self::with_options(Mp4FileMuxerOptions::default())
+    }
+
+    pub fn with_options(options: Mp4FileMuxerOptions) -> Self {
+        Self { options }
+    }
+}
