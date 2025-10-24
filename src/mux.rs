eeaf9c72b75458c669efc5b1ecd4e0556c4fef87
Takeru Ohta <phjgt308@gmail.com>
2025-10-24 10:30:41 +0900

    feat: mux モジュールに Sample と MuxError を追加し API を拡張

--- src/mux.rs
+++ src/mux.rs
@@ -1,17 +1,30 @@
 #![expect(missing_docs, dead_code)]
 
+use crate::Error;
+
 #[derive(Debug, Clone)]
 pub struct Mp4FileMuxerOptions {
+    pub audio_track: bool, // TODO: Option<TrackInfo>,
+    pub video_track: bool, // TODO: Option<TrackInfo>,
     pub reserved_moov_box_size: usize,
 }
 
-#[expect(clippy::derivable_impls)]
 impl Default for Mp4FileMuxerOptions {
     fn default() -> Self {
         Self {
+            audio_track: true,
+            video_track: true,
             reserved_moov_box_size: 0,
         }
     }
+}
+
+#[derive(Debug, Clone)]
+pub struct Sample {}
+
+#[derive(Debug)]
+pub enum MuxError {
+    EncodeError(Error),
 }
 
 #[derive(Debug)]
@@ -28,4 +41,14 @@ impl Mp4FileMuxer {
     pub fn with_options(options: Mp4FileMuxerOptions) -> Self {
         Self { options }
     }
+
+    pub fn header_bytes(&self) -> &[u8] {
+        todo!()
+    }
+
+    pub fn append_sample(&mut self, _sample: &Sample) -> Result<(), MuxError> {
+        todo!()
+    }
+
+    pub fn finalize(self) {}
 }
