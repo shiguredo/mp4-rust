396c6a69c8d0cc5d37d73c2bdeaf222b001e4364
Takeru Ohta <phjgt308@gmail.com>
2025-10-28 14:03:09 +0900

    feat: C APIにエラー型定義モジュールを追加

---
+++ crates/c-api/src/error.rs
@@ -0,0 +1,56 @@
+//! shiguredo_mp4 のエラーをまとめて定義するためのモジュール
+//!
+//! C API で細かくエラー方が分かれていると煩雑なので、ひとつに集約している
+use shiguredo_mp4::{
+    Error, ErrorKind, aux::SampleTableAccessorError, demux::DemuxError, mux::MuxError,
+};
+
+#[repr(C)]
+pub enum Mp4Error {
+    Ok = 0,
+    InvalidInput = 1,
+    InvalidData = 2,
+    InvalidState = 3,
+    InputRequired = 4,
+    Other = 100,
+}
+
+impl From<Error> for Mp4Error {
+    fn from(e: Error) -> Self {
+        match e.kind {
+            ErrorKind::InvalidInput => Self::InvalidInput,
+            ErrorKind::InvalidData => Self::InvalidData,
+            _ => Self::Other,
+        }
+    }
+}
+
+impl From<SampleTableAccessorError> for Mp4Error {
+    fn from(_e: SampleTableAccessorError) -> Self {
+        Self::InvalidData
+    }
+}
+
+impl From<DemuxError> for Mp4Error {
+    fn from(e: DemuxError) -> Self {
+        match e {
+            DemuxError::DecodeError(e) => e.into(),
+            DemuxError::SampleTableError(e) => e.into(),
+            DemuxError::InputRequired(_) => Self::InputRequired,
+            _ => Self::Other,
+        }
+    }
+}
+
+impl From<MuxError> for Mp4Error {
+    fn from(e: MuxError) -> Self {
+        match e {
+            MuxError::EncodeError(e) => e.into(),
+            MuxError::AlreadyFinalized => Self::InvalidState,
+            MuxError::PositionMismatch { .. }
+            | MuxError::MissingSampleEntry { .. }
+            | MuxError::SampleDurationOverflow { .. } => Self::InvalidInput,
+            _ => Self::Other,
+        }
+    }
+}
--- crates/c-api/src/lib.rs
+++ crates/c-api/src/lib.rs
@@ -1,3 +1,4 @@
 // #![no_std]
 pub mod basic_types;
 pub mod demux;
+pub mod error;
