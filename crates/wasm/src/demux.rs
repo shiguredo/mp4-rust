365f30239257c952151b39022d1c8f8b44a5fc2a
Takeru Ohta <phjgt308@gmail.com>
2026-01-09 14:41:49 +0900

    refactor: JSON フォーマット処理を関数に抽出し、サンプル変換関数の骨組みを追加する

--- crates/wasm/src/demux.rs
+++ crates/wasm/src/demux.rs
@@ -1,6 +1,6 @@
 //! C API の demux.rs に対応するモジュール
 use c_api::basic_types::Mp4TrackKind;
-use c_api::demux::Mp4DemuxTrackInfo;
+use c_api::demux::{Mp4DemuxSample, Mp4DemuxTrackInfo};
 
 /// トラック情報を JSON 文字列に変換する
 //
@@ -20,21 +20,33 @@ pub unsafe extern "C" fn mp4_demux_track_info_to_json(
     }
 
     let track_info = unsafe { &*track_info };
+    let json = nojson::json(|f| fmt_json_mp4_demux_track_info(f, track_info)).to_string();
+    Box::into_raw(Box::new(json.into_bytes()))
+}
 
+/// TODO: doc
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn mp4_demux_sample_to_json(sample: *const Mp4DemuxSample) -> *mut Vec<u8> {
+    if sample.is_null() {
+        return std::ptr::null_mut();
+    }
+
+    todo!()
+}
+
+fn fmt_json_mp4_demux_track_info(
+    f: &mut nojson::JsonFormatter<'_, '_>,
+    track_info: &Mp4DemuxTrackInfo,
+) -> std::fmt::Result {
     let kind = match track_info.kind {
         Mp4TrackKind::MP4_TRACK_KIND_AUDIO => "audio",
         Mp4TrackKind::MP4_TRACK_KIND_VIDEO => "video",
     };
 
-    let json = nojson::object(|f| {
+    f.object(|f| {
         f.member("track_id", track_info.track_id)?;
         f.member("kind", kind)?;
         f.member("duration", track_info.duration)?;
         f.member("timescale", track_info.timescale)
     })
-    .to_string();
-
-    Box::into_raw(Box::new(json.into_bytes()))
 }
-
-// mp4_demux_track_info_from_json()
